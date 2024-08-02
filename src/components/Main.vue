6c8e3f8a6be9512f1e5b55b6f6138de697eb4fa0
TΛRØTTΞR <399929@gmail.com>
2024-08-02 23:04:13 +0800

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -120,9 +120,9 @@
 
 
       <el-button style="float: right;margin-top: -20px;margin-right: 3px" type="primary" :icon="TrendCharts" link
-        v-if="!chartShow" @click="chartShow = true" />
+        v-if="chartShow" @click="chartShow = false" />
       <el-button style="float: right;margin-top: -20px;margin-right: 3px" type="primary" :icon="Hide" link
-        v-if="chartShow" @click="chartShow = true" />
+        v-if="chartShow" @click="chartShow = false" />
       <div v-show="chartShow" ref="chartContainer" style="width: 100%; height: 400px;"></div>
     </div>
   </div>
