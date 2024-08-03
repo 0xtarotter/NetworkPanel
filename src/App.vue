e9beed7c07d4df71fc13c5a1ab75911fdeb6c280
TΛRØTTΞR <399929@gmail.com>
2024-08-03 13:27:43 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -27,16 +27,21 @@
       <IPinfoUI :isVisible="isVisible" />
     </el-main>
     <div style="height: fit-content;padding-bottom: 10px;">
-      
-	    <center>
+      <div style="width: fit-content;margin-left: auto;margin-right: auto;">
+        <el-link type="info" @click='open("https://39MI.COM/")' target="_blank">
+		
+	<center>
   <a href="https://39mi.com" target="_blank" style="text-decoration:none;">    
 	<img src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0iaWNvbiIgdmlld0JveD0iMCAwIDEwMjQgMTAyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNDUiIGhlaWdodD0iNDUiPjxwYXRoIGQ9Ik01MTEuMTI1IDYzLjU2MmMyNDcuMDA3IDAgNDQ3LjI0OCAyMDAuMjQxIDQ0Ny4yNDggNDQ3LjI1IDAgMjQ3LjAxLTIwMC4yNDEgNDQ3LjI1LTQ0Ny4yNDggNDQ3LjI1LTI0Ny4wMSAwLTQ0Ny4yNTEtMjAwLjI0LTQ0Ny4yNTEtNDQ3LjI1IDAtMjQ3LjAwOSAyMDAuMjQtNDQ3LjI1IDQ0Ny4yNTEtNDQ3LjI1eiIgZmlsbD0iI0VFRSIvPjxwYXRoIGQ9Ik03MTEuNjc3IDkwNy44NzJjLTU5LjIxMyAyOC45MzYtMTI1LjczNyA0NS4yMi0xOTYuMDgxIDQ1LjIyLTg2LjcyNyAwLTE2Ny42MzUtMjQuNzYtMjM2LjE4OC02Ny40OSA3OS4yMzctMjAuNDY3IDE1NS44MjUtMTIuMjE0IDE3My4yMzQtNDguMTY2IDQwLjgxNi04MC4zMzcgMjAuNDE4LTExMC44Mi0zNi45NzQtMTIxLjE2OC03OC4wMy0yMC45NTItOTguOTcyLTU0LjgyOC04OS4xODQtMjUxLjk3OCAxOS4zMjUtMTA0LjQ0NCA0LjEtMzAzLjQxNCAyNDUuMDM2LTI2Ny40MDMgMjQ1Ljc0NyAzNi43NDYgMTcyLjIwNiAyNDYuODEgNjkuMjcyIDUwMC41NzQtNTIuODIyIDEzMC4yMzEgMjYuMDAzIDE4OC43MDYgNzAuODg1IDIxMC40MTF6IiBmaWxsPSIjRkZGIi8+PC9zdmc+" />
   <br />
-  <img src="https://s2.loli.net/2024/08/03/vbQgoYe7GOJuf9V.png" height="auto" width="150" >
+  <img src="https://s2.loli.net/2024/08/03/vbQgoYe7GOJuf9V.png" width="150" >
   <br />
   </a>
      <a style="font-size:10px;color:#dedede;">owner</a> 
-    </center> 
+    </center> 	
+	
+	</el-link>
+      </div>
 
     </div>
   </el-container>
